7198fe4c4e0b43471caa51deb2f5117067c44cad
ArjithaVivekanandan <36363436+ArjithaVivekanandan@users.noreply.github.com>
2021-08-14 09:59:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+const express = require('express')
+const app = express();
+const port = 8010;
+const fs=require('fs')
+
+app.use(express.json());
+
+
+ var d = new Date();
+ var timeStamp =""+d.getTime()+"";
+ var fileName="./"+d.getDate()+"-"+d.getHours()+".txt";
+
+
+ 
+app.get('/createFile', function (req, res) {
+    fs.writeFile(fileName,timeStamp,function(err){
+             if(err) throw err;   
+               res.json("File created")
+           })
+           
+  
+})
+
+app.get('/getTextFiles',function(req,res){
+    var files = fs.readdirSync("./");
+const path = require('path');
+const names = [];
+for(var i in files) {
+   if(path.extname(files[i]) === ".txt") {
+       names.push(files[i])
+   }
+}
+res.json(names)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+  });
+
+
+
+
